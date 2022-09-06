da43c5bcfd160ce00d44616d68a243b986928cba
Alejo Lovallo <alejoe.lovallo@gmail.com>
2022-09-05 23:28:50 -0300

    feat(linting): add linting

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,75 +1,75 @@
-const networks = require('./hardhat.networks');
+const networks = require('./hardhat.networks')
 
-require("@nomicfoundation/hardhat-toolbox");
-require('hardhat-deploy');
-require('hardhat-deploy-ethers');
-require('@nomiclabs/hardhat-etherscan');
-require('hardhat-abi-exporter');
-require('hardhat-gas-reporter');
-require('hardhat-docgen');
-require('hardhat-contract-sizer');
+require('@nomicfoundation/hardhat-toolbox')
+require('hardhat-deploy')
+require('hardhat-deploy-ethers')
+require('@nomiclabs/hardhat-etherscan')
+require('hardhat-abi-exporter')
+require('hardhat-gas-reporter')
+require('hardhat-docgen')
+require('hardhat-contract-sizer')
 
-const testnetAdmin = '0x080D834838dc9EE7154a5d13E03073CA2ADd0C92'; // Account 1
-const testnetUser1 = '0x09394bf9046485B043E4476dED219b3D718E6A90'; // Account 3
-const testnetUser2 = '0x9A669b35D0Fc83a3272A37aab7A3147c4dc5546b'; // Account 4
-const testnetUser3 = '0x8E0496C9786f8271Bc3712c9e5F794A137c2Dc87'; // Account 5
+const testnetAdmin = '0x080D834838dc9EE7154a5d13E03073CA2ADd0C92' // Account 1
+const testnetUser1 = '0x09394bf9046485B043E4476dED219b3D718E6A90' // Account 3
+const testnetUser2 = '0x9A669b35D0Fc83a3272A37aab7A3147c4dc5546b' // Account 4
+const testnetUser3 = '0x8E0496C9786f8271Bc3712c9e5F794A137c2Dc87' // Account 5
 
 const config = {
   solidity: {
-    version: "0.8.9",
+    version: '0.8.9',
     settings: {
       optimizer: {
         enabled: true,
-        runs: 1000,
-      },
-    },
+        runs: 1000
+      }
+    }
   },
   networks,
   gasReporter: {
     currency: 'USD',
     gasPrice: 1,
-    enabled: process.env.REPORT_GAS ? true : false,
+    enabled: process.env.REPORT_GAS ? true : false
   },
   namedAccounts: {
     deployer: {
-      default: 0,
+      default: 0
     },
     admin: {
       42: testnetAdmin,
       4: testnetAdmin,
-      3: testnetAdmin,
-    },  
+      3: testnetAdmin
+    },
     testnetUser1: {
       default: testnetUser1,
       3: testnetUser1,
       4: testnetUser1,
-      42: testnetUser1,
+      42: testnetUser1
     },
     testnetUser2: {
       default: testnetUser2,
       3: testnetUser2,
       4: testnetUser2,
-      42: testnetUser2,
+      42: testnetUser2
     },
     testnetUser3: {
       default: testnetUser3,
       3: testnetUser3,
       4: testnetUser3,
-      42: testnetUser3,
-    },    
+      42: testnetUser3
+    }
   },
   etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY,
+    apiKey: process.env.ETHERSCAN_API_KEY
   },
   mocha: {
-    timeout: 30000,
-  },    
+    timeout: 30000
+  },
   abiExporter: {
     path: './abis',
     runOnCompile: true,
     clear: true,
-    flat: true,
-  },    
-}; 
+    flat: true
+  }
+}
 
-module.exports = config;
+module.exports = config
--- hardhat.networks.js
+++ hardhat.networks.js
@@ -1,84 +1,81 @@
-require('dotenv').config
-
 const accounts = process.env.PRIVATE_KEY
   ? [process.env.PRIVATE_KEY]
   : {
-      mnemonic: process.env.HDWALLET_MNEMONIC,
-    };
+      mnemonic: process.env.HDWALLET_MNEMONIC
+    }
 
 const networks = {
   coverage: {
     url: 'http://127.0.0.1:8555',
     blockGasLimit: 200000000,
-    allowUnlimitedContractSize: true,
+    allowUnlimitedContractSize: true
   },
   localhost: {
     chainId: 1337,
     url: 'http://127.0.0.1:8545',
     allowUnlimitedContractSize: true,
-    timeout: 1000 * 60,
-  },
-};
+    timeout: 1000 * 60
+  }
+}
 
 if (process.env.ALCHEMY_URL && process.env.FORK_ENABLED) {
   networks.hardhat = {
     allowUnlimitedContractSize: true,
     chainId: 1,
     forking: {
-      url: process.env.ALCHEMY_URL,
+      url: process.env.ALCHEMY_URL
     },
-    accounts,
-  };
+    accounts
+  }
   if (process.env.FORK_BLOCK_NUMBER) {
-    networks.hardhat.forking.blockNumber = parseInt(process.env.FORK_BLOCK_NUMBER);
+    networks.hardhat.forking.blockNumber = parseInt(process.env.FORK_BLOCK_NUMBER)
   }
 } else {
   networks.hardhat = {
-    allowUnlimitedContractSize: true,
-  };
+    allowUnlimitedContractSize: true
+  }
 }
 
 if (process.env.HDWALLET_MNEMONIC || process.env.PRIVATE_KEY) {
   networks.mumbai = {
     chainId: 80001,
     url: 'https://rpc-mumbai.maticvigil.com',
-    accounts,
-  };
+    accounts
+  }
 }
 
 if (process.env.POLYGONSCAN_API_KEY && (process.env.HDWALLET_MNEMONIC || process.env.PRIVATE_KEY)) {
-   networks.matic = {
+  networks.matic = {
     chainId: 137,
     url: `https://polygon-mainnet.g.alchemy.com/v2/${process.env.POLYGONSCAN_API_KEY}`,
-    accounts,
-   };
+    accounts
+  }
 } else {
-    console.warn('No polygonscan or hdwallet available for testnets');
+  console.warn('No polygonscan or hdwallet available for testnets')
 }
-
 
 if (process.env.INFURA_API_KEY && (process.env.HDWALLET_MNEMONIC || process.env.PRIVATE_KEY)) {
   networks.kovan = {
     url: `https://kovan.infura.io/v3/${process.env.INFURA_API_KEY}`,
-    accounts,
-  };
+    accounts
+  }
 
   networks.ropsten = {
     url: `https://ropsten.infura.io/v3/${process.env.INFURA_API_KEY}`,
-    accounts,
-  };
+    accounts
+  }
 
   networks.rinkeby = {
     url: `https://rinkeby.infura.io/v3/${process.env.INFURA_API_KEY}`,
-    accounts,
-  };
+    accounts
+  }
 
   networks.mainnet = {
     url: `https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
-    accounts,
-  };
+    accounts
+  }
 } else {
-  console.warn('No infura or hdwallet available for testnets');
+  console.warn('No infura or hdwallet available for testnets')
 }
 
-module.exports = networks;
+module.exports = networks
