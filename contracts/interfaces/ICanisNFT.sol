e930598d5481400aa25ae76f4c345c6830537db7
Alejo Lovallo <alejoe.lovallo@gmail.com>
2022-09-13 00:04:50 -0300

    feat(royalties): wip contract

---
+++ contracts/interfaces/ICanisNFT.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/common/ERC2981.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+interface CanisNFT {}
