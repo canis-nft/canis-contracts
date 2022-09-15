138c5cb03c6c538eec7be0943744bb5126d415f4
Alejo Lovallo <alejoe.lovallo@gmail.com>
2022-09-15 09:11:13 -0300

    wip tests

--- test/SwapBurner.js
+++ test/SwapBurner.js
@@ -24,6 +24,61 @@ describe('Swap Burner', function () {
 
   it('Should have initialized correctly', async () => {
     //GIVEN
+    const expectedUniswapAddress = this.uniswapRouter.address
+    const expectedUBIToken = this.UBI.address
+    const expectedOwner = this.deployer
+    //WHEN
+    const uniswapRouter = await this.swapBurner.Uniswap()
+    const ubiToken = await this.swapBurner.UBI()
+    const owner = await this.swapBurner.owner()
+    //THEN
+    expect(expectedUniswapAddress).to.be.equal(uniswapRouter)
+    expect(expectedUBIToken).to.be.equal(ubiToken)
+    expect(owner).to.be.equal(this.deployer)
+  })
+
+  it('Should be able to approve UniswapRouter', async () => {
+    //GIVEN
+    const initialAllowance = await this.UBI.allowance(this.swapBurner.address, this.uniswapRouter.address)
+    const allowance = '1000000'
+    //WHEN
+    await this.swapBurner.approveUniSwap(allowance)
+    const finalAllowance = await this.UBI.allowance(this.swapBurner.address, this.uniswapRouter.address)
+    //THEN
+    expect(initialAllowance).to.be.equal(0)
+    expect(finalAllowance).to.be.equal(allowance)
+  })
+
+  it('Should be able to increase allowance to UniswapRouter', async () => {
+    //GIVEN
+    const allowance = '1000000'
+    await this.swapBurner.approveUniSwap(allowance)
+    const initialAllowance = await this.UBI.allowance(this.swapBurner.address, this.uniswapRouter.address)
+    const toAddAllowance = '50'
+    //WHEN
+    await this.swapBurner.increaseUniswapAllowance(toAddAllowance)
+    const finalAllowance = await this.UBI.allowance(this.swapBurner.address, this.uniswapRouter.address)
+    //THEN
+    expect(initialAllowance).to.be.equal(allowance)
+    expect(finalAllowance).to.be.equal('1000050')
+  })
+
+  it('Should be able to decrease allowance to UniswapRouter', async () => {
+    //GIVEN
+    const allowance = '1000050'
+    await this.swapBurner.approveUniSwap(allowance)
+    const initialAllowance = await this.UBI.allowance(this.swapBurner.address, this.uniswapRouter.address)
+    const toDecreaseAllowance = '50'
+    //WHEN
+    await this.swapBurner.decreaseUniswapAllowance(toDecreaseAllowance)
+    const finalAllowance = await this.UBI.allowance(this.swapBurner.address, this.uniswapRouter.address)
+    //THEN
+    expect(initialAllowance).to.be.equal(allowance)
+    expect(finalAllowance).to.be.equal('1000000')
+  })
+
+  it('Should be able to receive ether, swap and burn UBI tokens', async () => {
+    //GIVEN
     //WHEN
     //THEN
   })
